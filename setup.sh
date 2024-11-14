8c966f7d88899706671e9ed9f7769e82658ce6ad
thierry-rami <thierry.rami@laplateforme.io>
2024-11-14 11:37:29 +0100

    ajout setup

---
+++ setup.sh
@@ -0,0 +1,18 @@
+sudo apt install -y git python3-venv python3-pyscard build-essential libpcsclite-dev python3-dev python3-tk  -y
+cd
+git clone https://github.com/RubenHabibLaPlateforme/Badgeuse_Activity.git
+cd Badgeuse_Activity
+python3 -m venv ~/Badgeuse
+patch controllers/App.py ~/badgeuse/app.patch
+patch requirements.txt ~/badgeuse/requirements.patch
+source ~/Badgeuse/bin/activate
+pip3 install -r requirements.txt 
+cd ~/badgeuse
+chmod +x 
+sed -i "s/xxxxx/$USER/g" Badgeuse.desktop 
+chmod +x Badgeuse.sh
+cp Badgeuse.sh ../Badgeuse_Activity
+cp Badgeuse.png ../Badgeuse_Activity
+sudo cp Badgeuse.desktop /usr/share/applications/Badgeuse.desktop 
+cd
+rm ~/badgeuse -fr
